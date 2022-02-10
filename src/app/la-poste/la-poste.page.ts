fea911f66aebe37d116194f399cb68d1ab0d0fd1
Lavia <samy.langlois@gmail.com>
2022-02-10 19:52:07 +0100

    working on input line

--- src/app/la-poste/la-poste.page.ts
+++ src/app/la-poste/la-poste.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonDatetime } from '@ionic/angular';
+import { format, parseISO } from 'date-fns';
+import { fr } from 'date-fns/locale';
+
 
 @Component({
   selector: 'app-la-poste',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./la-poste.page.scss'],
 })
 export class LaPostePage implements OnInit {
+  @ViewChild(IonDatetime, { static: true }) datetime: IonDatetime;
+
+  dateValue = '';
+  dateValue2 = '';
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  confirm() {
+    this.datetime.confirm();
+  }
+  
+  reset() {
+    this.datetime.reset()
+  }
+
+  formatDate(value: string) {
+    return format(parseISO(value), 'dd/MM/yyyy', {locale: fr});
+  }
+
 
 }
